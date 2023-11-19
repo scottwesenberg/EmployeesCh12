427011b41d863b49d64046a7209fef39719075ec
scottwesenberg <scowesen@gmail.com>
2023-11-19 15:57:55 -0500

    Add project files.

---
+++ Models/DepartmentLocation.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeesCh12.Models
+{
+    public class DepartmentLocation
+    {
+        public int DepartmentID { get; set; }
+        public int LocationID { get; set; }
+
+        //navigation properties
+        public Department Department { get; set; } = null!;
+        public Location Location { get; set; } = null!;
+    }
+}
